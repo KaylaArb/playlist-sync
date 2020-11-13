7d7561047865df02181610b9413908a2f032b4fb
KaylaArb <35358725+KaylaArb@users.noreply.github.com>
2020-11-13 14:06:21 +0100

    inital commit

---
+++ src/main/java/com/example/playlistsync/PlaylistSyncApplication.java
@@ -0,0 +1,13 @@
+package com.example.playlistsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaylistSyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/playlistsync/PlaylistSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.playlistsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
