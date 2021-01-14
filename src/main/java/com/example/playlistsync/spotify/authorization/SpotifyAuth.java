ba44a03cfc1feb2baf8436a34b0397c1aa128cb1
KaylaArb <35358725+KaylaArb@users.noreply.github.com>
2021-01-14 13:19:58 +0100

    adjusted URI code

--- src/main/java/com/example/playlistsync/spotify/authorization/SpotifyAuth.java
+++ src/main/java/com/example/playlistsync/spotify/authorization/SpotifyAuth.java
@@ -10,7 +10,7 @@ public class SpotifyAuth {
 
     private static final String clientId = "f0e7c7f529464a69b6011881f225d657";
     private static final String clientSecret = "78fc07921bc3403d937c2a7551bdfdeb";
-    private static final URI redirectUri = SpotifyHttpManager.makeUri("http://localhost:8080/api/get-user-code");
+    private static final URI redirectUri = SpotifyHttpManager.makeUri("https://playlist-sync-backend.herokuapp.com/api/get-user-code");
     public static String code = "";
 
     public static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
